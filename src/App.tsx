d418ca9303324226c1cf7760ca159ba99b51be8e
imibeetle <imibeetle@gmail.com>
2024-01-21 12:01:55 +0330

    Add: Input fields to further calculate the delivery fee

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [cartValue, setCartValue] = useState<number>(0);
+  const [deliveryDistance, setDeliveryDistance] = useState<number>(0);
+  const [numberOfItems, setNumberOfItems] = useState<number>(0);
+  const [orderTime, setOrderTime] = useState<string>('');
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +24,32 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <input
+          type="number"
+          value={cartValue}
+          onChange={(e) => setCartValue(Number(e.target.value))}
+          placeholder="Cart Value"
+        />
+        <input
+          type="number"
+          value={deliveryDistance}
+          onChange={(e) => setDeliveryDistance(Number(e.target.value))}
+          placeholder="Delivery Distance (in meters)"
+        />
+        <input
+          type="number"
+          value={numberOfItems}
+          onChange={(e) => setNumberOfItems(Number(e.target.value))}
+          placeholder="Number of Items"
+        />
+        <input
+          type="text"
+          value={orderTime}
+          onChange={(e) => setOrderTime(e.target.value)}
+          placeholder="Order Time (as text for now)"
+        />
+      </div>
     </div>
   );
 }
